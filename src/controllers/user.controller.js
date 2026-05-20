01aac24ddfa867aae13541a78720cdb283bba9d1
Divya <bellamkonda2004@gmail.com>
2026-05-20 22:08:59 +0530

    add getUserChannelProfile function to retrieve user channel details with subscription info

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -252,6 +252,73 @@ const updateCoverImage=asyncHandler(async(req,res)=>{
   )
 })
 
+
+
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+  const {username}=req.params;
+  if(!username?.trim()){
+    throw new ApiError(401,"User name not found")
+  }
+
+  const channel=await User.aggregate([
+    {
+      $match:{
+        username:username?.toLowerCase()
+      }
+    },
+    {
+        $lookup:{
+          from:"subscription",
+          localField:"_id",
+          foreignField:"channel",
+          as:"subscribers"
+        }
+      },{
+        $lookup:{
+          from:"subscription",
+          localField:"_id",
+          foreignField:"subscriber",
+          as:"subscribedTo"
+        }
+      },{
+        $addFields:{
+          subscribersCount:{
+            $size:"$subscribers"
+          },
+          channelsSubscriberToCount:{
+            $size:"$subscribedTo"
+          },
+          isSubscribed:{
+            $cond:{
+              if:{$in:[req.user?._id,"subscribers.subscriber"]},
+              then:true,
+              else:false
+            }
+          }
+        }
+      },{
+        $project:{
+          fullName:1,
+          username:1,
+          subscriberCount:1,
+          channelsSubscriberToCount:1,
+          isSubscribed:1,
+          avatar:1,
+          coverImage:1,
+          email:1
+        }
+      }
+  ])
+  if(!channel?.length)
+    throw new ApiError(404,"channel does not exists");
+
+  return res.status(200)
+  .json(
+    new ApiResponse(200,channel[0],"User Channel Fetched successfully")
+  )
+
+})
+
 export {
   registerUser,
   loginUser,
@@ -261,5 +328,6 @@ export {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
-  updateCoverImage
+  updateCoverImage,
+  getUserChannelProfile
 };
